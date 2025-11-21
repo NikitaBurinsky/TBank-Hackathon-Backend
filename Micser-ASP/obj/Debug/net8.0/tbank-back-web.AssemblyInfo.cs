32ce0484b00f825707ad8f0ae1b0e428d4aa5e09
Nikita Burinsky <nikburinsky@mail.ru>
2025-11-21 23:50:18 +0300

    ms-1 8000:8000

--- Micser-ASP/obj/Debug/net8.0/tbank-back-web.AssemblyInfo.cs
+++ Micser-ASP/obj/Debug/net8.0/tbank-back-web.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tbank-back-web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2be700ebd554696147f2d79b0ba97cbf88c168c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+373a63916b33df70722ceb27d463e7e075a0dad4")]
 [assembly: System.Reflection.AssemblyProductAttribute("tbank-back-web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tbank-back-web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
