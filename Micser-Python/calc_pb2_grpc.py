ba9c484e57af945b78f17d6afc5fd3548fe1ce96
Egor <egoryurenka@gmail.com>
2025-11-22 16:26:38 +0300

    fixed req files in Python

--- Micser-Python/calc_pb2_grpc.py
+++ Micser-Python/calc_pb2_grpc.py
@@ -4,25 +4,6 @@ import grpc
 
 import calc_pb2 as calc__pb2
 
-GRPC_GENERATED_VERSION = '1.76.0'
-GRPC_VERSION = grpc.__version__
-_version_not_supported = False
-
-try:
-    from grpc._utilities import first_version_is_lower
-    _version_not_supported = first_version_is_lower(GRPC_VERSION, GRPC_GENERATED_VERSION)
-except ImportError:
-    _version_not_supported = True
-
-if _version_not_supported:
-    raise RuntimeError(
-        f'The grpc package installed is at version {GRPC_VERSION},'
-        + ' but the generated code in calc_pb2_grpc.py depends on'
-        + f' grpcio>={GRPC_GENERATED_VERSION}.'
-        + f' Please upgrade your grpc module to grpcio>={GRPC_GENERATED_VERSION}'
-        + f' or downgrade your generated code using grpcio-tools<={GRPC_VERSION}.'
-    )
-
 
 class CalculatorStub(object):
     """Описание сервиса
@@ -38,7 +19,7 @@ class CalculatorStub(object):
                 '/calc.Calculator/Add',
                 request_serializer=calc__pb2.AddRequest.SerializeToString,
                 response_deserializer=calc__pb2.AddReply.FromString,
-                _registered_method=True)
+                )
 
 
 class CalculatorServicer(object):
@@ -63,7 +44,6 @@ def add_CalculatorServicer_to_server(servicer, server):
     generic_handler = grpc.method_handlers_generic_handler(
             'calc.Calculator', rpc_method_handlers)
     server.add_generic_rpc_handlers((generic_handler,))
-    server.add_registered_method_handlers('calc.Calculator', rpc_method_handlers)
 
 
  # This class is part of an EXPERIMENTAL API.
@@ -82,18 +62,8 @@ class Calculator(object):
             wait_for_ready=None,
             timeout=None,
             metadata=None):
-        return grpc.experimental.unary_unary(
-            request,
-            target,
-            '/calc.Calculator/Add',
+        return grpc.experimental.unary_unary(request, target, '/calc.Calculator/Add',
             calc__pb2.AddRequest.SerializeToString,
             calc__pb2.AddReply.FromString,
-            options,
-            channel_credentials,
-            insecure,
-            call_credentials,
-            compression,
-            wait_for_ready,
-            timeout,
-            metadata,
-            _registered_method=True)
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
